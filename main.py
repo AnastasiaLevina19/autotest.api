1b4ee00ecafe79e02a4b4b116ebfae1633053ca0
AnastasiaLevina19 <nastasiya.levina@auxo.team>
2026-04-02 19:08:51 +0300

    GitHub Desktop

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print('Hello from GitHub Desktop!')
     print('Hello from GitHub Desktop!123')
+    print('Hello from GitHub Desktop!12345')
 
 
 # Press the green button in the gutter to run the script.
